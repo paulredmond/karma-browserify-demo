fb9b76e7dd02f3bb176eceb1300bc8804698f9df
Paul Redmond <paulrredmond@gmail.com>
2015-07-29 17:02:45 -0700

    More backbone test examples.

--- src/DemoBackboneView.js
+++ src/DemoBackboneView.js
@@ -1,7 +1,15 @@
 module.exports = Backbone.View.extend({
   template: _.template("<a href=\"http://test.com\"><%= linkText %></a>"),
+  events: {
+    "click a": "linkClicked"
+  },
   render: function() {
     this.$el.html(this.template({ linkText: "Test 123" }));
     return this;
+  },
+  linkClicked: function(e) {
+    e.preventDefault();
+    this.$el.append(_.template("<p class=\"log\"><%= text %></p>")({ text: "link clicked!"}));
+    this.trigger('log:appended', this.$('p.log').length);
   }
 });
--- test/spec/BackboneExampleSpec.js
+++ test/spec/BackboneExampleSpec.js
@@ -6,14 +6,17 @@ var fixture = require("../test_helper").Fixture;
  * and make some assertions.
  *
  * Not meant to replace or replicate e2e tests!
+ * This might not even be needed for simple view tests...see the second test
  */
 describe('Example backbone view', function() {
+  var View = require('../../src/DemoBackboneView');
   var subject;
 
   beforeEach(fixture.beforeEach);
   beforeEach(function() {
-    var Subject = require('../../src/DemoBackboneView');
-    subject = new Subject({});
+    subject = new View({
+      el: fixture.getContainer()
+    });
   });
 
   afterEach(fixture.afterEach);
@@ -23,5 +26,25 @@ describe('Example backbone view', function() {
     expect(subject.$el.prop('tagName').toLowerCase()).to.equal("div");
     var $link = subject.$el.find('a').first();
     expect($link.html()).to.equal("Test 123");
+  });
+
+  it('can pass without a fixture container in the DOM', function() {
+    subject = new View({});
+    subject.render();
+    expect(subject.$el.prop('tagName').toLowerCase()).to.equal("div");
+    expect(subject.$el.parent().length).to.equal(0);
+    var $link = subject.$el.find('a').first();
+    expect($link.html()).to.equal("Test 123");
+  });
+
+  it ('can log clicks to the view', function(done) {
+    subject.render();
+    subject.on("log:appended", function(count) {
+      expect(count).to.equal(1);
+      expect(subject.$el.find('p').html()).to.equal("link clicked!");
+      done();
+    });
+
+    subject.$el.find('a').first().trigger('click');
   });
 });
